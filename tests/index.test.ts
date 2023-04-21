843104a7caf5ee094b213ca7898eb087887b34ec
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2023-04-21 12:27:20 +0100

    callback returned

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,8 +3,8 @@ import app from "../src/server";
 
 describe("Test /agent route", () => {
   it("GET: /agents should return 200 response", () => {
-    request(app).get("/api/v1/agent/test", (data) => {
-      expect(data.statusCode).toBe(200);
+   return  request(app).get("/api/v1/agent/test", (data) => {
+     return  expect(data.statusCode).toBe(200);
     });
   });
 });
