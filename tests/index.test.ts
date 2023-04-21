ffc48d2a8d3232a213ab5e9fa1afe6f0f5f41a34
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2023-04-21 13:01:38 +0100

    promis

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,7 +3,14 @@ import app from "../src/server";
 
 describe("Test /agent route", () => {
   it("GET: /agents should return 200 response", async () => {
-    const res = await request(app).get("/api/v1/agent/test");
-    expect(res.statusCode).toBe(200);
+    //  return  request(app).get("/api/v1/agent/test", (data) => {
+    //    return  expect(data.statusCode).toBe(200);
+    //   });
+
+    request(app)
+      .get("/api/v1/agent/test")
+      .then((data) => {
+        expect(data.statusCode).toBe(200);
+      });
   });
 });
