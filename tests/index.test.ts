5cf01cb1d75d9e6d5d24904a9900606abf20ec71
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2023-04-21 12:16:50 +0100

    cicd error

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,15 +2,8 @@ import request from "supertest";
 import app from "../src/server";
 
 describe("Test /agent route", () => {
-  it("GET: /agents should return 200 response", () => {
-    request(app)
-      .get("/api/v1/agent/test")
-      .then((res) => {
-        expect(res.statusCode).toBe(200);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
+  it("GET: /agents should return 200 response", async () => {
+    const res = await request(app).get("/api/v1/agent/test");
+    expect(res.statusCode).toBe(200);
   });
-  return;
 });
