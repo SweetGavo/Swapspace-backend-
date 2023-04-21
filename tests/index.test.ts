c6aa08730d6299c34366ba55c1ba87dc4bf462cd
dogubo mamuzo joshua <dogubo.joshua@decagon.dev>
2023-04-21 12:54:33 +0100

    async-await

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,9 +2,8 @@ import request from "supertest";
 import app from "../src/server";
 
 describe("Test /agent route", () => {
-  it("GET: /agents should return 200 response", () => {
-   return  request(app).get("/api/v1/agent/test", (data) => {
-     return  expect(data.statusCode).toBe(200);
-    });
+  it("GET: /agents should return 200 response", async () => {
+    const res = await request(app).get("/api/v1/agent/test");
+    expect(res.statusCode).toBe(200);
   });
 });
