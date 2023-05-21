201186ecb042005c7a15c97e5e62fae70b0a78fe
gbubemi22 <slimnfine22@gmail.com>
2023-05-21 02:16:11 +0100

    fix uploads-to-cloud

--- src/router/rectorRouter.ts
+++ src/router/rectorRouter.ts
@@ -1,14 +1,44 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import multer from 'multer';
 const router = express.Router();
 
 
 
 import rectorController from '../controllers/rectorController';
 
+//Set up multer storage and file filter
+const storage = multer.diskStorage({});
+const fileFilter = (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+     const allowedMimeTypes = [
+          'image/jpg',
+          'image/gif',
+          'image/jpeg',
+          'image/png',
+          'application/pdf',
+          'application/msword',
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+     ];
+     if (!allowedMimeTypes.includes(file.mimetype)) {
+          const error: any = new Error('Please provide a valid image, PDF, or document file');
+          error.code = 'LIMIT_UNEXPECTED_FILE';
+          return cb(error, false);
+     }
+     cb(null, true);
+};
 
-router
-.route('/')
-.post (rectorController.createAgentProfile);
+// Create a multer upload instance
+const upload = multer({
+     storage,
+     fileFilter,
+     limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+
+
+
+router.route('/').post(upload.array('broker_card_image', 2), rectorController.createAgentProfile);
+
+router.route('/image-upload').post(upload.single('image'),rectorController.updateAgentProfileImage)
 
 
 
