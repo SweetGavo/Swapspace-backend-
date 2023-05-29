b5fa3ccbe166dd649313372cf295ffcb47f7f4ea
gbubemi22 <slimnfine22@gmail.com>
2023-05-29 09:09:48 +0100

    fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,16 +1,18 @@
 import CopyWebpackPlugin from 'copy-webpack-plugin';
-import path from 'path';
+
 
 export default {
-  // Other Webpack configuration options...
+  
   plugins: [
-    // Other plugins...
+    
     new CopyWebpackPlugin({
       patterns: [
         {
           from: 'prisma',
           to: 'dist/prisma',
           noErrorOnMissing: true,
+          force: true ,
+          info: { minimized: true }
         },
       ],
     }),
