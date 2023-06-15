284069eb10809927901feb9f203cbfc745c4e9e1
gbubemi22 <slimnfine22@gmail.com>
2023-06-15 09:45:37 +0100

    work

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express';
 import prisma from '../DB/prisma';
 import { StatusCodes } from 'http-status-codes';
+import createTaskSchema from '../utils/taskValidator'
 
 const taskController = {
   createTask: async (req: Request, res: Response): Promise<Response> => {
+      
+    const { error, value } = createTaskSchema.validate(req.body);
+  if (error) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ error: error.details[0].message });
+  }
+
+
     const {
       title,
       action,
-      assignee,
+      assignee ,
       collaborator,
       number_of_deals,
       contact,
