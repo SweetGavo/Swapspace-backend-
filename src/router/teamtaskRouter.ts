189836c84d290f9e806905ee0d8418ca4de3022f
gbubemi22 <slimnfine22@gmail.com>
2023-06-16 12:50:43 +0100

    fix

--- src/router/teamtaskRouter.ts
+++ src/router/teamtaskRouter.ts
@@ -1,16 +1,13 @@
-import express  from 'express';
+import express from 'express';
 const router = express.Router();
 
-
-import  teamTaskController from '../controllers/teamtaskController';
-
-
-
-
+import teamTaskController from '../controllers/teamtaskController';
 
 router
-.route('/')
-.post(teamTaskController.createTeamTask)
+  .route('/')
+  .post(teamTaskController.createTeamTask)
+  .get(teamTaskController.getAllTeamTasks);
 
+router.route('/realtor/:realtorId').get(teamTaskController.getTasksByRealtor);
 
 export default router;
